62a770e9a3275bde1e1f031b12f8a05e5d4ebfc7
Adachi <rin.adachi1999@gmail.com>
2019-11-15 17:31:15 +0900

    エディタ拡張用のウィンドウ作成

---
+++ Ice Scate/Assets/Editor/PositionEditWindow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PositionEditWindow : EditorWindow
+{
+    private int id = 0;
+    private int padding = 20;
+
+    [MenuItem("PositionEditor/PositionEditWindow")]
+    static void ShowWindow()
+    {
+        GetWindow<PositionEditWindow>();
+    }
+
+    void OnGUI()
+    {
+        //文字列を張り付ける
+        EditorGUILayout.LabelField("項目を選んでください");
+
+        GUILayout.Space(padding);
+
+        //入力用のフィールド
+        id = EditorGUILayout.IntField("番号",id);
+
+        //項目間の余白
+        GUILayout.Space(padding);
+
+        //ボタンの表示と実行処理
+        if (GUILayout.Button("セーブ", GUILayout.Height(30)))
+        {
+            Debug.Log("セーブ");
+        }
+
+        GUILayout.Space(padding);
+
+        if (GUILayout.Button("ロード", GUILayout.Height(30)))
+        {
+            Debug.Log("ロード");
+        }
+    }
+}
