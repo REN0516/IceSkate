e26b53f2846bdac97e06df6f741b0c3a10a13eaa
ren0516 <ren.numao0516@gmail.com>
2019-11-22 16:07:58 +0900

    画面遷移アニメーション実装

---
+++ Ice Scate/Assets/LoadObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadObject : MonoBehaviour
+{
+
+    public static LoadObject transition_;
+    [SerializeField] Fade fade = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (transition_ == null)
+        {
+            transition_ = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Fade(string name)
+    {
+        fade.FadeIn(0.3f, () =>
+        {
+            SceneManager.LoadScene(name);
+            fade.FadeOut(0.3f);
+        });
+        //SceneManager.LoadScene(name);
+    }
+
+
+
+}
